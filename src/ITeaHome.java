38e5dd740e71541fd4215f0a3d9bf847ac62fd4e
george neamt <georgeneamt2004@yahoo.com>
2019-11-26 20:33:20 +0200

    This is the second commit

--- src/ITeaHome.java
+++ src/ITeaHome.java
@@ -2,5 +2,6 @@ public class ITeaHome {
 
     public static void main(String[] args){
         System.out.println("Hello ITeaHome Academy! George salutes you!");
+        System.out.println("This is the second line and it will be the second commit");
     }
 }
